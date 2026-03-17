f83e7337f09238250b7b8819e9ad99acef793ac6
qrsm2250050-dotcom <qrsm2250050@slu.edu.ph>
2026-03-17 21:53:26 +0800

    Initial commit

---
+++ src/main/java/me/qrae/randomicity/Randomicity.java
@@ -0,0 +1,15 @@
+package me.qrae.randomicity;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Randomicity implements ModInitializer {
+	public static final String MOD_ID = "randomicity";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/me/qrae/randomicity/RandomicityClient.java
@@ -0,0 +1,10 @@
+package me.qrae.randomicity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomicityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
